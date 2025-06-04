0ff683b5fbdb08f7a36bdb85a3865257bbd8e1ac
Galia Almaraz <galia.almaraz@uab.edu.bo>
2025-06-04 11:13:03 -0400

    armando las estructura de carpetas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: [
+    "./*.html",                
+    "./src/pages/**/*.html"     
+  ],
   theme: {
     extend: {},
   },
